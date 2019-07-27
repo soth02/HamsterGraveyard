803ed8114be219f5a2506f145497b44d7b1bd665
soth02 <cpfong@gmail.com>
2019-07-27 14:50:47 -0700

    update with HamsterGraveyard

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var HamsterGraveyad = artifacts.require("./HamsterGraveyard.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(HamsterGraveyard);
 };
