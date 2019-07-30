c3c92f701b0ff751f99db5d94c175670d800ead5
soth02 <cpfong@gmail.com>
2019-07-29 19:15:10 -0700

    added isPetOwner modifier

--- contracts/HamsterGraveyard.sol
+++ contracts/HamsterGraveyard.sol
@@ -3,24 +3,35 @@ pragma solidity ^0.5.0;
 import "../installed_contracts/Ownable.sol";
 import "../installed_contracts/Pausable.sol";
 
+///@title A virtual graveyard for beloved hamsters
+///@author Soth02
+///@notice Use this contract at your own risk!  It has not been professionally audited for security.
 contract HamsterGraveyard is Ownable, Pausable {
 
-    /*
-        Create a variable to keep track of the event ID numbers.
-    */
+
+    ///    Create a variable to keep track of the hamsterGrave ID numbers.
     uint public idGenerator = 0;
 
+    /// this is the main data structure for instantiated a single hamster memorial.  The front end UI will display this information.
     struct HamsterGrave {
       string name;
+
+      /// should validate these for valid years.  This input validation can be done on the front end as well.
       uint yearOfBirth;
       uint yearOfDeath;
 
-      //limit to 100 characters
+      /// limit to 100 characters
       string memoriam;
+      bool isCreated;
     }
 
+    /// owners stores a mapping of individual owner addresses to
     mapping (address => mapping (uint => HamsterGrave)) public owners;
 
+    modifier isPetOwner(address _address, uint hamsterGraveNum){
+      require( owners[msg.sender][hamsterGraveNum].isCreated == true);
+      _;
+     }
 
     event LogHamsterGraveAdded(uint hamsterGraveNum, string name, uint yearOfBirth, uint yearOfDeath, string memoriam);
     event LogHamsterGraveUpdated(uint hamsterGraveNum, string name, uint yearOfBirth, uint yearOfDeath, string memoriam);
@@ -29,7 +40,7 @@ contract HamsterGraveyard is Ownable, Pausable {
       return (idGenerator);
     }
 
-    //
+    ///
     function addHamsterGrave (string memory name, uint yearOfBirth, uint yearOfDeath, string memory memoriam ) public whenNotPaused
         returns (uint) {
 
@@ -39,6 +50,7 @@ contract HamsterGraveyard is Ownable, Pausable {
         owners[msg.sender][idGenerator].yearOfBirth = yearOfBirth;
         owners[msg.sender][idGenerator].yearOfDeath = yearOfDeath;
         owners[msg.sender][idGenerator].memoriam = memoriam;
+        owners[msg.sender][idGenerator].isCreated = true;
 
         emit LogHamsterGraveAdded(idGenerator, name, yearOfBirth, yearOfDeath, memoriam);
 
@@ -47,8 +59,8 @@ contract HamsterGraveyard is Ownable, Pausable {
 	      return (idGenerator-1);
     }
 
-    //update the dog info
-    function updateHamsterGrave (uint hamsterGraveNum, string memory name, uint yearOfBirth, uint yearOfDeath, string memory memoriam ) public whenNotPaused
+    ///update the HamsterGrave info
+    function updateHamsterGrave (uint hamsterGraveNum, string memory name, uint yearOfBirth, uint yearOfDeath, string memory memoriam ) public whenNotPaused isPetOwner(msg.sender, hamsterGraveNum)
         returns (uint) {
 
         owners[msg.sender][hamsterGraveNum].name = name;
@@ -61,7 +73,7 @@ contract HamsterGraveyard is Ownable, Pausable {
 	      return (hamsterGraveNum);
     }
 
-    //
+    ///
     function viewHamsterGrave (uint hamsterGraveNum) view public
 	  returns (string memory name, uint yearOfBirth, uint yearOfDeath, string memory memoriam) {
 
--- test/hamsterGraveyard.test.js
+++ test/hamsterGraveyard.test.js
@@ -1,19 +1,29 @@
 const HamsterGraveyard = artifacts.require("./HamsterGraveyard.sol");
 const BN = web3.utils.BN;
+let catchRevert = require("./exceptionsHelpers.js").catchRevert;
 
 contract("HamsterGraveyard", accounts => {
 
 
   const owner = accounts[0];
+  const alice = accounts[1];
+  const bob = accounts[2];
+
   const name = "totoro";
   const yob = 2001;
   const yod = 2002;
   const memoriam = "RIP";
 
+  const updatedName = "Hammy";
+  const updatedYob = 2003;
+  const updatedYod = 2004;
+  const updatedMemoriam = "later";
+
   beforeEach(async () => {
     instance = await HamsterGraveyard.new();
   })
 
+  //this test is validating that we are correctly iterating the idGenerator after a HamsterGrave is added
   it("idGenerator should be iterated by 1 after addHamsterGrave is called.", async () => {
 
     const idGeneratorInitial = await instance.getIdGenerator.call();
@@ -22,6 +32,7 @@ contract("HamsterGraveyard", accounts => {
     assert.equal(idGeneratorInitial.toNumber()+1, idGeneratorNext.toNumber(), "idGeneratorInitial+1 should equal idGeneratorNext.");
   })
 
+  //this test validates that we are correctly setting a HamsterGrave's id
   it("the id of an added HamsterGrave should set to idGenerator.", async () => {
 
     const idGeneratorInitial = await instance.getIdGenerator.call();
@@ -31,6 +42,7 @@ contract("HamsterGraveyard", accounts => {
     assert.equal(idGeneratorInitial.toNumber(), graveData.hamsterGraveNum.toNumber(), "idGeneratorInitial should equal the hamsterGraveNum.");
   })
 
+  //this test validates that a HamsterGrave is added with the correct arguments
   it("adding a hamsterGrave should emit an event with the provided details.", async () => {
 
     const tx = await instance.addHamsterGrave(name, yob, yod, memoriam);
@@ -42,18 +54,23 @@ contract("HamsterGraveyard", accounts => {
     assert.equal(memoriam, graveData.memoriam, "the added memoriam should match");
   })
 
+  //this test validates that updating a HamsterGrave changes to expected values.
   it("updating a hamsterGrave should change the values for a hamsterGrave", async () => {
 
     const tx = await instance.addHamsterGrave(name, yob, yod, memoriam);
     const graveData = tx.logs[0].args;
 
-    const tx2 = await instance.updateHamsterGrave(graveData.hamsterGraveNum,'Hammy', 2003, 2004, 'later');
+    const tx2 = await instance.updateHamsterGrave(graveData.hamsterGraveNum,updatedName, updatedYob, updatedYod, updatedMemoriam);
     const updatedGraveData = tx2.logs[0].args;
 
     assert.notEqual(graveData.name, updatedGraveData.name, "the updated name should be changed");
     assert.notEqual(graveData.yearOfBirth.toNumber(), updatedGraveData.yearOfBirth.toNumber(), "the updated yearOfBirth should be changed");
     assert.notEqual(graveData.yearOfDeath.toNumber(), updatedGraveData.yearOfDeath.toNumber(),"the updated yearOfDeath should be changed");
     assert.notEqual(graveData.memoriam, updatedGraveData.memoriam,"the updated memoriam should be changed");
+    assert.equal(updatedName, updatedGraveData.name, "the updated name should match");
+    assert.equal(updatedYob, updatedGraveData.yearOfBirth.toNumber(), "the updated yearOfBirth should match");
+    assert.equal(updatedYod, updatedGraveData.yearOfDeath.toNumber(), "the updated yearOfDeath should match");
+    assert.equal(updatedMemoriam, updatedGraveData.memoriam, "the updated memoriam should match");
   })
 
   it("viewing a hamsterGrave should correctly return its values", async () => {
@@ -69,8 +86,12 @@ contract("HamsterGraveyard", accounts => {
     assert.equal(graveData.memoriam, response.memoriam,"the viewed memoriam should be equal to the added Memoriam");
   })
 
-  it("future test", async () => {
+  it("you should only be able to update your own memorials", async () => {
 
+    const tx = await instance.addHamsterGrave(name, yob, yod, memoriam, {from: owner});
+    const graveData = tx.logs[0].args;
+
+    await catchRevert(instance.updateHamsterGrave(graveData.hamsterGraveNum,updatedName, updatedYob, updatedYod, updatedMemoriam, {from:alice}));
   })
 
 })
