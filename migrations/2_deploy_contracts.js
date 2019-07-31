e6b8423f20ec3dce83c426969a8be1b6942044ae
soth02 <cpfong@gmail.com>
2019-07-31 07:04:08 -0700

    deployed to rinkeby

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var HamsterGraveyard = artifacts.require("./HamsterGraveyard.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
   deployer.deploy(HamsterGraveyard);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+const MNEMONIC = 'MNEMONIC';
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +10,20 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/KEY")
+      },
+      network_id: 3,
+      gas: 4000000
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://rinkeby.infura.io/v3/KEY")
+      },
+      network_id: 4,
+      gas: 4000000
     }
   }
 };
