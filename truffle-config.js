138a1aba590cfbd317b6109590ee288fee87476c
soth02 <cpfong@gmail.com>
2019-08-04 19:37:22 -0700

    removing truffle-hdwallet-provider for now

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-var HDWalletProvider = require("truffle-hdwallet-provider");
+//var HDWalletProvider = require("truffle-hdwallet-provider");
 
 const MNEMONIC = 'MNEMONIC';
 
